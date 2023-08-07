879060c48126da992a50bdda25c7591c28b6d7d2
alfexyz <alexxflo28@gmail.com>
2023-08-07 17:35:50 +0300

    solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    let sum = 0;
+    if(start > stop){
+        let a = stop;
+        stop = start;
+        start = a;
+    }
+    if(typeof(start) === 'number' && typeof(stop) ==='number' && start >= 0 && stop >= 0){
+        for(let i = start; i <= stop; i ++){
+            sum += i;
+        }
+        return sum;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
