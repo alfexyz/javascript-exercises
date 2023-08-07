9d85e6a3f99f066edb214ea6d72a3b65c242a505
alfexyz <alexxflo28@gmail.com>
2023-08-07 17:30:36 +0300

    solved exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    const arg = [...arguments];
+    let arr = arg[0];
+    arg.shift();
+    return arr.filter((x) =>{
+        for(let i = 0; i < arg.length; i ++){
+            if(x === arg[i]) return false;
+        }
+        return true;
+    });
 };
 
 // Do not edit below this line
